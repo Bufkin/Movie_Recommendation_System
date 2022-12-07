2aa443f14fcf7c86ea2761085e463d46992c5581
Sebastian Schemer <sebastianschemer@pm.me>
2022-12-07 22:14:41 +0100

    Fixed all comments in the Python file

--- Movie_Recommendation_System.py
+++ Movie_Recommendation_System.py
@@ -2,37 +2,36 @@ import numpy as np
 from lightfm.datasets import fetch_movielens
 from lightfm import LightFM
 
-#fetch data and format it
+# fetch data and format it
 data = fetch_movielens(min_rating=4.0)
 
-#print training and testing data
+# print training and testing data
 print(repr(data['train']))
 print(repr(data['test']))
 
 
-#create model
+# create model
 model = LightFM(loss='warp')
-#train model
+# train model
 model.fit(data['train'], epochs=30, num_threads=2)
 
 
 def sample_recommendation(model, data, user_ids):
-
-    #number of users and movies in training data
+    # number of users and movies in training data
     n_users, n_items = data['train'].shape
 
-    #generate recommendations for each user we input
+    # generate recommendations for each user we input
     for user_id in user_ids:
 
-        #movies they already like
+        # movies they already like
         known_positives = data['item_labels'][data['train'].tocsr()[user_id].indices]
 
-        #movies our model predicts they will like
+        # movies our model predicts they will like
         scores = model.predict(user_id, np.arange(n_items))
-        #rank them in order of most liked to least
+        # rank them in order of most liked to least
         top_items = data['item_labels'][np.argsort(-scores)]
 
-        #print out the results
+        # print out the results
         print("User %s" % user_id)
         print("     Known positives:")
 
